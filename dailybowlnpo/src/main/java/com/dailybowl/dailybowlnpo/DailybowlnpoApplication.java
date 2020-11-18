56a9d63309a5b7d6d590b2f8fe64ebc2a25cb1e8
yhs97 <sarvaiyayash@gmail.com>
2020-11-17 19:13:49 -0700

    Created Project Structure

---
+++ dailybowlnpo/src/main/java/com/dailybowl/dailybowlnpo/DailybowlnpoApplication.java
@@ -0,0 +1,13 @@
+package com.dailybowl.dailybowlnpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailybowlnpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailybowlnpoApplication.class, args);
+	}
+
+}
---
+++ dailybowlnpo/src/test/java/com/dailybowl/dailybowlnpo/DailybowlnpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailybowl.dailybowlnpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailybowlnpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
